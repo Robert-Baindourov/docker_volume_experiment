c53ba03532a5f929dc08990a77f5bcf279d491ed
rbaindourov <rbaindourov@gmail.com>
2019-09-19 16:28:54 -0700

    unexpected results, changing order of volume command, next

--- experiment_one.sh
+++ experiment_one.sh
@@ -1,4 +1,4 @@
 rm -rf /tmp/all_boxes
-docker run -itd --name buster_one -v /tmp/all_boxes:/var/www buster_one:latest
-docker run -itd --name buster_two -v /tmp/all_boxes:/var/www buster_two:latest
+docker run -itd  -v /tmp/all_boxes:/var/www buster_one:latest
+docker run -itd  -v /tmp/all_boxes:/var/www buster_two:latest
 
